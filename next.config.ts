5006b90457e883178e6ef4bab10510bd9e4fe8c6
jayantp2003 <jayantparakh2003@gmail.com>
2025-03-22 09:23:15 +0530

    Second try

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Enables static export
+  basePath: "/dashboard", // Replace with your GitHub repo name
+  assetPrefix: "/dashboard/",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
